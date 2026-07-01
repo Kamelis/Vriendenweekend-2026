b6455c434be8e84d9b94f2a29b5659e623edfa2e
Kamelis <n.vervoort@gmail.com>
2026-07-01 23:40:54 +0200

    Add artist images to bibliography

--- README.md
+++ README.md
@@ -7,9 +7,10 @@ Static programme and photo site for Rock Werchter 2026.
 - Main page: `index.html`
 - Photo page: `photos.html`
 - Shared photo data: `photos.js`
+- Artist images: `artist-images.js` with Wikimedia thumbnails where available and generated fallback tiles elsewhere
 - Hero asset: `artwork/rock-werchter-hero.jpg`
 - Photo assets: `artwork/werchter-upload-001.jpg`, `artwork/werchter-upload-002.jpg`, `artwork/werchter-upload-003.jpg`, `artwork/werchter-upload-004.jpg`
-- Current site version: `2.6.1`
+- Current site version: `2.7.0`
 - Artist bibliography: generated for every act in the programme, with Spotify links for known songs or top-song searches
 
 ## Telegram photo workflow
@@ -24,3 +25,7 @@ When a loose photo is sent via Telegram in this chat, treat it as a photo upload
 6. Bump the version and deploy to `main`.
 
 Uncertain matches should be marked `te-controleren`.
+
+## Artist images
+
+Artist cards use `artist-images.js`. The current pass found 65 Wikimedia/Wikipedia thumbnails for 144 programme acts; acts without a reliable match render a generated fallback tile with the artist name.
---
+++ artist-images.js
@@ -0,0 +1,335 @@
+window.werchterArtistImages = {
+  "A Perfect Circle": {
+    "src": "https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/A_Perfect_Circle_Lollapalooza_Chile_2013.jpg/330px-A_Perfect_Circle_Lollapalooza_Chile_2013.jpg",
+    "source": "https://en.wikipedia.org/wiki/A_Perfect_Circle",
+    "title": "A Perfect Circle"
+  },
+  "Agnes Obel": {
+    "src": "https://upload.wikimedia.org/wikipedia/commons/thumb/2/26/Agnes_Obel2_Sentrum_Scene_20205_%28cropped%29.jpg/330px-Agnes_Obel2_Sentrum_Scene_20205_%28cropped%29.jpg",
+    "source": "https://en.wikipedia.org/wiki/Agnes_Obel",
+    "title": "Agnes Obel"
+  },
+  "All Them Witches": {
+    "src": "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1a/All_Them_Witches_Press_Photo.jpg/330px-All_Them_Witches_Press_Photo.jpg",
+    "source": "https://en.wikipedia.org/wiki/All_Them_Witches",
+    "title": "All Them Witches"
+  },
+  "Beirut": {
+    "src": "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/Beyrouth_vue_a%C3%A9rienne.jpg/330px-Beyrouth_vue_a%C3%A9rienne.jpg",
+    "source": "https://en.wikipedia.org/wiki/Beirut",
+    "title": "Beirut"
+  },
+  "Blood Incantation": {
+    "src": "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/Blood_Incantation_live.jpg/330px-Blood_Incantation_live.jpg",
+    "source": "https://en.wikipedia.org/wiki/Blood_Incantation",
+    "title": "Blood Incantation"
+  },
+  "Boy & Girl": {
+    "src": "https://upload.wikimedia.org/wikipedia/en/a/aa/Boy_%26_Girl_%282003_TV_show%29_poster.jpg",
+    "source": "https://en.wikipedia.org/wiki/Boy_%26_Girl",
+    "title": "Boy & Girl"
+  },
+  "Charlotte Adigéry": {
+    "src": "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/Visit_of_the_College_of_Commissioners_Egmont_Palace%2C_Belgian_Presidency_of_the_Council_of_the_European_Union_%28EU2024BE%29_on_5_January_2024_-_243.jpg/330px-Visit_of_the_College_of_Commissioners_Egmont_Palace%2C_Belgian_Presidency_of_the_Council_of_the_European_Union_%28EU2024BE%29_on_5_January_2024_-_243.jpg",
+    "source": "https://en.wikipedia.org/wiki/Charlotte_Adig%C3%A9ry",
+    "title": "Charlotte Adigéry"
+  },
+  "Charlotte de Witte": {
+    "src": "https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Charlotte_de_witte-1513626416.jpg/330px-Charlotte_de_witte-1513626416.jpg",
+    "source": "https://en.wikipedia.org/wiki/Charlotte_de_Witte",
+    "title": "Charlotte de Witte"
+  },
+  "CMAT": {
+    "src": "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4b/CMAT%2C_May_2025_%28cropped%29.jpg/330px-CMAT%2C_May_2025_%28cropped%29.jpg",
+    "source": "https://en.wikipedia.org/wiki/CMAT",
+    "title": "CMAT"
+  },
+  "Comrad": {
+    "src": "https://upload.wikimedia.org/wikipedia/commons/thumb/2/27/Harrison_Fisher_WWI_American_Red_Cross_poster.jpg/330px-Harrison_Fisher_WWI_American_Red_Cross_poster.jpg",
+    "source": "https://en.wikipedia.org/wiki/Comrade",
+    "title": "Comrade"
+  },
+  "Cory Wong": {
+    "src": "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Cory_Wong%2C_Fredrikstad_%28Norway%29%2C_2_April_2022_%28cropped%29.jpg/330px-Cory_Wong%2C_Fredrikstad_%28Norway%29%2C_2_April_2022_%28cropped%29.jpg",
+    "source": "https://en.wikipedia.org/wiki/Cory_Wong",
+    "title": "Cory Wong"
+  },
+  "David Byrne": {
+    "src": "https://upload.wikimedia.org/wikipedia/commons/thumb/5/54/David_Byrne_San_Diego.jpg/330px-David_Byrne_San_Diego.jpg",
+    "source": "https://en.wikipedia.org/wiki/David_Byrne",
+    "title": "David Byrne"
+  },
+  "Dylan Gossett": {
+    "src": "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9c/Dylan_Gossett_at_O2_Ritz%2C_Manchester.png/330px-Dylan_Gossett_at_O2_Ritz%2C_Manchester.png",
+    "source": "https://en.wikipedia.org/wiki/Dylan_Gossett",
+    "title": "Dylan Gossett"
+  },
+  "Ethel Cain": {
+    "src": "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/Ethel_Cain_at_Gunnersbury_Park_%282023-08-20%29_01_%28cropped%29.jpg/330px-Ethel_Cain_at_Gunnersbury_Park_%282023-08-20%29_01_%28cropped%29.jpg",
+    "source": "https://en.wikipedia.org/wiki/Ethel_Cain",
+    "title": "Ethel Cain"
+  },
+  "FKA twigs": {
+    "src": "https://upload.wikimedia.org/wikipedia/commons/thumb/4/46/FKA_twigs_grammy_%28cropped%29.jpg/330px-FKA_twigs_grammy_%28cropped%29.jpg",
+    "source": "https://en.wikipedia.org/wiki/FKA_Twigs",
+    "title": "FKA Twigs"
+  },
+  "Florence Road": {
+    "src": "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1a/Florence_Road_performing_in_Cardiff.jpg/330px-Florence_Road_performing_in_Cardiff.jpg",
+    "source": "https://en.wikipedia.org/wiki/Florence_Road",
+    "title": "Florence Road"
+  },
+  "Franz Ferdinand": {
+    "src": "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Ferdinand_Schmutzer_-_Franz_Ferdinand_von_%C3%96sterreich-Este%2C_um_1914_%28cropped%29.jpg/330px-Ferdinand_Schmutzer_-_Franz_Ferdinand_von_%C3%96sterreich-Este%2C_um_1914_%28cropped%29.jpg",
+    "source": "https://en.wikipedia.org/wiki/Archduke_Franz_Ferdinand_of_Austria",
+    "title": "Archduke Franz Ferdinand of Austria"
+  },
+  "Gorillaz": {
+    "src": "https://upload.wikimedia.org/wikipedia/en/thumb/c/c6/Gorillaz_family.jpg/330px-Gorillaz_family.jpg",
+    "source": "https://en.wikipedia.org/wiki/Gorillaz",
+    "title": "Gorillaz"
+  },
+  "HAEVN": {
+    "src": "https://upload.wikimedia.org/wikipedia/commons/thumb/2/28/HAEVN_%40Carr%C3%A9.jpg/330px-HAEVN_%40Carr%C3%A9.jpg",
+    "source": "https://en.wikipedia.org/wiki/Haevn",
+    "title": "Haevn"
+  },
+  "Harry Mack": {
+    "src": "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b7/Harry_Mack_at_Yankees_clubhouse_in_Tampa.jpg/330px-Harry_Mack_at_Yankees_clubhouse_in_Tampa.jpg",
+    "source": "https://en.wikipedia.org/wiki/Harry_Mack",
+    "title": "Harry Mack"
+  },
+  "Henri PFR": {
+    "src": "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9c/Henri_pfr-1522706406_%28cropped%29.jpg/330px-Henri_pfr-1522706406_%28cropped%29.jpg",
+    "source": "https://en.wikipedia.org/wiki/Henri_PFR",
+    "title": "Henri PFR"
+  },
+  "Jessie Murph": {
+    "src": "https://upload.wikimedia.org/wikipedia/commons/thumb/0/04/Jessie_Murph.jpg/330px-Jessie_Murph.jpg",
+    "source": "https://en.wikipedia.org/wiki/Jessie_Murph",
+    "title": "Jessie Murph"
+  },
+  "Joy Crookes": {
+    "src": "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f9/Joy_Crookes-5992.jpg/330px-Joy_Crookes-5992.jpg",
+    "source": "https://en.wikipedia.org/wiki/Joy_Crookes",
+    "title": "Joy Crookes"
+  },
+  "Kae Tempest": {
+    "src": "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b0/LRdR2024KaeTempest_2.jpg/330px-LRdR2024KaeTempest_2.jpg",
+    "source": "https://en.wikipedia.org/wiki/Kae_Tempest",
+    "title": "Kae Tempest"
+  },
+  "Kasabian": {
+    "src": "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/Kasabian_-_18.10.2021_-_The_Engine_Shed%2C_Lincoln_%2853402154824%29.jpg/330px-Kasabian_-_18.10.2021_-_The_Engine_Shed%2C_Lincoln_%2853402154824%29.jpg",
+    "source": "https://en.wikipedia.org/wiki/Kasabian",
+    "title": "Kasabian"
+  },
+  "Kneecap": {
+    "src": "https://upload.wikimedia.org/wikipedia/commons/thumb/0/09/Knee_diagram.svg/330px-Knee_diagram.svg.png",
+    "source": "https://en.wikipedia.org/wiki/Patella",
+    "title": "Patella"
+  },
+  "La Fuente": {
+    "src": "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0e/Unilever_Distributionszentrum_Mannheim.jpg/330px-Unilever_Distributionszentrum_Mannheim.jpg",
+    "source": "https://en.wikipedia.org/wiki/List_of_Unilever_brands",
+    "title": "List of Unilever brands"
+  },
+  "LANDMVRKS": {
+    "src": "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/Landmvrks_Full_Force_2019_09.jpg/330px-Landmvrks_Full_Force_2019_09.jpg",
+    "source": "https://en.wikipedia.org/wiki/Landmvrks",
+    "title": "Landmvrks"
+  },
+  "Lauren Spencer Smith": {
+    "src": "https://upload.wikimedia.org/wikipedia/commons/thumb/7/79/Lauren_Spencer_Smith_2023.png/330px-Lauren_Spencer_Smith_2023.png",
+    "source": "https://en.wikipedia.org/wiki/Lauren_Spencer_Smith",
+    "title": "Lauren Spencer Smith"
+  },
+  "Lewis Capaldi": {
+    "src": "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e6/Glasto2025-41_%28cropped%29.jpg/330px-Glasto2025-41_%28cropped%29.jpg",
+    "source": "https://en.wikipedia.org/wiki/Lewis_Capaldi",
+    "title": "Lewis Capaldi"
+  },
+  "Loyle Carner": {
+    "src": "https://upload.wikimedia.org/wikipedia/commons/thumb/0/04/Loyle_Carner_-_Superbloom-Festival_2024_-_DSC1932.jpg/330px-Loyle_Carner_-_Superbloom-Festival_2024_-_DSC1932.jpg",
+    "source": "https://en.wikipedia.org/wiki/Loyle_Carner",
+    "title": "Loyle Carner"
+  },
+  "Maraschino": {
+    "src": "https://upload.wikimedia.org/wikipedia/en/thumb/f/f3/Maraschino_cocktail.jpeg/330px-Maraschino_cocktail.jpeg",
+    "source": "https://en.wikipedia.org/wiki/Maraschino",
+    "title": "Maraschino"
+  },
+  "Matt Berninger": {
+    "src": "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Matt_Berninger_2019_%28cropped%29.jpg/330px-Matt_Berninger_2019_%28cropped%29.jpg",
+    "source": "https://en.wikipedia.org/wiki/Matt_Berninger",
+    "title": "Matt Berninger"
+  },
+  "MATTN": {
+    "src": "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e5/MATTN_%40_Veltins-Arena_in_Germany.png/330px-MATTN_%40_Veltins-Arena_in_Germany.png",
+    "source": "https://en.wikipedia.org/wiki/Anouk_Matton",
+    "title": "Anouk Matton"
+  },
+  "Mirage": {
+    "src": "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e9/Farallon_Islands_at_inferior_mirage_no_mirage_and_superior_mirage.jpg/330px-Farallon_Islands_at_inferior_mirage_no_mirage_and_superior_mirage.jpg",
+    "source": "https://en.wikipedia.org/wiki/Mirage",
+    "title": "Mirage"
+  },
+  "Moby": {
+    "src": "https://upload.wikimedia.org/wikipedia/commons/thumb/5/54/Moby_12_17_2018_-16_%2831743191557%29.jpg/330px-Moby_12_17_2018_-16_%2831743191557%29.jpg",
+    "source": "https://en.wikipedia.org/wiki/Moby",
+    "title": "Moby"
+  },
+  "Mogwai": {
+    "src": "https://upload.wikimedia.org/wikipedia/commons/thumb/0/02/Mogwai_rock_city_140223.jpg/330px-Mogwai_rock_city_140223.jpg",
+    "source": "https://en.wikipedia.org/wiki/Mogwai",
+    "title": "Mogwai"
+  },
+  "Monza": {
+    "src": "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/Reggia_di_Monza.jpg/330px-Reggia_di_Monza.jpg",
+    "source": "https://en.wikipedia.org/wiki/Monza",
+    "title": "Monza"
+  },
+  "Mumford & Sons": {
+    "src": "https://upload.wikimedia.org/wikipedia/commons/thumb/2/23/Mumford_%26_Sons%2C_performing_at_Aviemore%2C_Scotland_2015.jpg/330px-Mumford_%26_Sons%2C_performing_at_Aviemore%2C_Scotland_2015.jpg",
+    "source": "https://en.wikipedia.org/wiki/Mumford_%26_Sons",
+    "title": "Mumford & Sons"
+  },
+  "NewDad": {
+    "src": "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2e/Newdad_Dome_2025.jpg/330px-Newdad_Dome_2025.jpg",
+    "source": "https://en.wikipedia.org/wiki/NewDad",
+    "title": "NewDad"
+  },
+  "overpass": {
+    "src": "https://upload.wikimedia.org/wikipedia/commons/thumb/1/17/I-73_at_I-840_Overpasses_Aerial_-_Facing_South_%2850698132631%29.jpg/330px-I-73_at_I-840_Overpasses_Aerial_-_Facing_South_%2850698132631%29.jpg",
+    "source": "https://en.wikipedia.org/wiki/Overpass",
+    "title": "Overpass"
+  },
+  "Palaye Royale": {
+    "src": "https://upload.wikimedia.org/wikipedia/commons/thumb/4/49/Palaye_Royale.jpg/330px-Palaye_Royale.jpg",
+    "source": "https://en.wikipedia.org/wiki/Palaye_Royale",
+    "title": "Palaye Royale"
+  },
+  "Palmera": {
+    "src": "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2b/Esglesia_Palmera.jpg/330px-Esglesia_Palmera.jpg",
+    "source": "https://en.wikipedia.org/wiki/Palmera",
+    "title": "Palmera"
+  },
+  "Paris Paloma": {
+    "src": "https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/Paris_Paloma_%40_Hotel_Cafe_06_08_2023_%2853048173943%29.jpg/330px-Paris_Paloma_%40_Hotel_Cafe_06_08_2023_%2853048173943%29.jpg",
+    "source": "https://en.wikipedia.org/wiki/Paris_Paloma",
+    "title": "Paris Paloma"
+  },
+  "Paul Kalkbrenner": {
+    "src": "https://upload.wikimedia.org/wikipedia/commons/thumb/8/83/Paul_Kalkbrenner_2016_%281_von_3%29_%28cropped%29.jpg/330px-Paul_Kalkbrenner_2016_%281_von_3%29_%28cropped%29.jpg",
+    "source": "https://en.wikipedia.org/wiki/Paul_Kalkbrenner",
+    "title": "Paul Kalkbrenner"
+  },
+  "Psychonaut": {
+    "src": "https://upload.wikimedia.org/wikipedia/commons/thumb/3/39/GoldFlwr3.gif/330px-GoldFlwr3.gif",
+    "source": "https://en.wikipedia.org/wiki/Psychonautics",
+    "title": "Psychonautics"
+  },
+  "Reneé Rapp": {
+    "src": "https://upload.wikimedia.org/wikipedia/commons/thumb/2/21/Renee_Rapp_at_KCAs_2024.jpg/330px-Renee_Rapp_at_KCAs_2024.jpg",
+    "source": "https://en.wikipedia.org/wiki/Rene%C3%A9_Rapp",
+    "title": "Reneé Rapp"
+  },
+  "Rise Against": {
+    "src": "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f8/Rise_Against_-_2023153205444_2023-06-02_Rock_am_Ring_-_Sven_-_1D_X_MK_II_-_3381_-_AK8I5374.jpg/330px-Rise_Against_-_2023153205444_2023-06-02_Rock_am_Ring_-_Sven_-_1D_X_MK_II_-_3381_-_AK8I5374.jpg",
+    "source": "https://en.wikipedia.org/wiki/Rise_Against",
+    "title": "Rise Against"
+  },
+  "Scene Queen": {
+    "src": "https://upload.wikimedia.org/wikipedia/commons/thumb/5/56/Scene_Queen_Warsaw_27042024.jpg/330px-Scene_Queen_Warsaw_27042024.jpg",
+    "source": "https://en.wikipedia.org/wiki/Scene_Queen",
+    "title": "Scene Queen"
+  },
+  "Sierra Ferrell": {
+    "src": "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7d/Sierra_Ferrell.jpg/330px-Sierra_Ferrell.jpg",
+    "source": "https://en.wikipedia.org/wiki/Sierra_Ferrell",
+    "title": "Sierra Ferrell"
+  },
+  "Social Distortion": {
+    "src": "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/Social_Distortion_2011-12-11_04.jpg/330px-Social_Distortion_2011-12-11_04.jpg",
+    "source": "https://en.wikipedia.org/wiki/Social_Distortion",
+    "title": "Social Distortion"
+  },
+  "Teddy Swims": {
+    "src": "https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Teddy_Swims_Warner_Records.jpg/330px-Teddy_Swims_Warner_Records.jpg",
+    "source": "https://en.wikipedia.org/wiki/Teddy_Swims",
+    "title": "Teddy Swims"
+  },
+  "The Cure": {
+    "src": "https://upload.wikimedia.org/wikipedia/commons/thumb/2/21/The_Cure_Live_in_Singapore_2-_1st_August_2007.jpg/330px-The_Cure_Live_in_Singapore_2-_1st_August_2007.jpg",
+    "source": "https://en.wikipedia.org/wiki/The_Cure",
+    "title": "The Cure"
+  },
+  "The Last Dinner Party": {
+    "src": "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e4/The_Last_Dinner_Party%2C_Circuit%2C_Kingston.jpg/330px-The_Last_Dinner_Party%2C_Circuit%2C_Kingston.jpg",
+    "source": "https://en.wikipedia.org/wiki/The_Last_Dinner_Party",
+    "title": "The Last Dinner Party"
+  },
+  "The Lumineers": {
+    "src": "https://upload.wikimedia.org/wikipedia/commons/thumb/0/02/The_Lumineers_performing_at_Wrigley_Field_in_2022.jpg/330px-The_Lumineers_performing_at_Wrigley_Field_in_2022.jpg",
+    "source": "https://en.wikipedia.org/wiki/The_Lumineers",
+    "title": "The Lumineers"
+  },
+  "The New Eves": {
+    "src": "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3e/The_New_Eves_%2854694650073%29.jpg/330px-The_New_Eves_%2854694650073%29.jpg",
+    "source": "https://en.wikipedia.org/wiki/The_New_Eves",
+    "title": "The New Eves"
+  },
+  "The Prodigy": {
+    "src": "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fe/The_Prodigy_IMG_2972_%285353883317%29_%28cropped%29.jpg/330px-The_Prodigy_IMG_2972_%285353883317%29_%28cropped%29.jpg",
+    "source": "https://en.wikipedia.org/wiki/The_Prodigy",
+    "title": "The Prodigy"
+  },
+  "The Reytons": {
+    "src": "https://upload.wikimedia.org/wikipedia/commons/thumb/9/94/The_Reytons.jpg/330px-The_Reytons.jpg",
+    "source": "https://en.wikipedia.org/wiki/The_Reytons",
+    "title": "The Reytons"
+  },
+  "The Vaccines": {
+    "src": "https://upload.wikimedia.org/wikipedia/commons/thumb/8/85/The_Vaccines_2023.jpg/330px-The_Vaccines_2023.jpg",
+    "source": "https://en.wikipedia.org/wiki/The_Vaccines",
+    "title": "The Vaccines"
+  },
+  "The xx": {
+    "src": "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cb/The_xx_-_Ilosaarirock_2012.jpg/330px-The_xx_-_Ilosaarirock_2012.jpg",
+    "source": "https://en.wikipedia.org/wiki/The_xx",
+    "title": "The xx"
+  },
+  "Twenty One Pilots": {
+    "src": "https://upload.wikimedia.org/wikipedia/commons/thumb/a/aa/Twenty_One_Pilots_from_Clancy_Tour_in_Dublin_%28landscape_crop%29.jpg/330px-Twenty_One_Pilots_from_Clancy_Tour_in_Dublin_%28landscape_crop%29.jpg",
+    "source": "https://en.wikipedia.org/wiki/Twenty_One_Pilots",
+    "title": "Twenty One Pilots"
+  },
+  "Viagra Boys": {
+    "src": "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/SXSW_2019_-_Viagra_Boys_%2847437549961%29.jpg/330px-SXSW_2019_-_Viagra_Boys_%2847437549961%29.jpg",
+    "source": "https://en.wikipedia.org/wiki/Viagra_Boys",
+    "title": "Viagra Boys"
+  },
+  "Westside Cowboy": {
+    "src": "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/Westside_Cowboy_at_Clwb_Ifor_Bach_Nov_2025.jpg/330px-Westside_Cowboy_at_Clwb_Ifor_Bach_Nov_2025.jpg",
+    "source": "https://en.wikipedia.org/wiki/Westside_Cowboy",
+    "title": "Westside Cowboy"
+  },
+  "Wolf Alice": {
+    "src": "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/Wolf_Alice_%40_Grammy_Museum_10_27_2021_%2851782701242%29.jpg/330px-Wolf_Alice_%40_Grammy_Museum_10_27_2021_%2851782701242%29.jpg",
+    "source": "https://en.wikipedia.org/wiki/Wolf_Alice",
+    "title": "Wolf Alice"
+  },
+  "Yard Act": {
+    "src": "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bf/Yard_Act_at_Rough_Trade_%2851833745502%29.jpg/330px-Yard_Act_at_Rough_Trade_%2851833745502%29.jpg",
+    "source": "https://en.wikipedia.org/wiki/Yard_Act",
+    "title": "Yard Act"
+  }
+};
+
+window.werchterArtistImageMeta = {
+  source: "Wikipedia/Wikimedia page thumbnails where available",
+  generatedAt: "2026-07-01T21:39:23.859Z",
+  found: 65,
+  total: 144,
+  fallback: "Generated artist-name tile for acts without a reliable Wikimedia thumbnail"
+};
